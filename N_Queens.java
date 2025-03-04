ac575d8214829821c98a3921ee8f67c43dec2a5a
Mansi Mehra <mansi2004mehra18@gmail.com>
2025-03-05 00:29:26 +0530

    add modificaions

--- N_Queens.java
+++ N_Queens.java
@@ -1,23 +1,24 @@
 
 public class N_Queens {
+
     public static boolean isSafe(char board[][], int row, int col) {
         //vertical up
-        for(int i=row-1; i>=0; i--) {
-            if(board[i][col] == 'Q') {
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
                 return false;
             }
         }
 
         //diag left up
-        for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--){
-            if(board[i][j] == 'Q') {
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
                 return false;
             }
         }
 
         //diag right up
-        for(int i=row-1, j=col+1; i>=0 && j<board.length; i--, j++) {
-            if(board[i][j] == 'Q') {
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
                 return false;
             }
         }
@@ -29,16 +30,17 @@ public class N_Queens {
     public static void nQueens(char board[][], int row) {
         //base
         if (row == board.length) {
-            printBoard(board);
+            // printBoard(board);
+            count++;
             return;
         }
         //column loop
         for (int j = 0; j < board.length; j++) {
-            if(isSafe(board, row, j)){
+            if (isSafe(board, row, j)) {
                 board[row][j] = 'Q';
                 nQueens(board, row + 1);//function call
                 board[row][j] = '.';//backtracking step
-            }          
+            }
         }
     }
 
@@ -52,6 +54,8 @@ public class N_Queens {
         }
     }
 
+    static int count = 0;
+
     public static void main(String args[]) {
         int n = 4;
         char board[][] = new char[n][n];
@@ -63,5 +67,6 @@ public class N_Queens {
         }
 
         nQueens(board, 0);
+        System.out.println("total ways to solve n queens = " + count);
     }
 }
